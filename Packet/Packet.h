23e6782749afa98e03c537144e7885412ff7e3bc
Louis OBryan <obryanlouis@gmail.com>
2013-11-02 16:22:52 -0700

    Added basic functionality to packets.

--- Packet/Packet.h
+++ Packet/Packet.h
@@ -3,13 +3,30 @@
 #ifndef PACKET_H
 #define PACKET_H
 
-enum PacketType {
-    INF,
-    DATA,
-    ACK
-};
-
+#include "Flow.h"
 class Packet{
+    private:
+        enum PacketType {
+            INF,
+            DATA,
+            ACK
+        };
+        char *data;
+        PacketType type;
+        string id;
+        Host *source;
+        Host *destination;
+        time_t startTime;
+        int size;
+        Flow *flowId;
+
+    public:
+        PacketType getType();
+        Packet(char *d, PacketType t, Host *s, Host *de, time_t start, 
+                int size, Flow *f);
+        ~Packet();
+        const char * getData();
+        void setData(char * d);
 
 };
 
