e62684c38dbaa606fe888246a7a8b38727bc8a9a
Victor Duan <victor.w.duan@gmail.com>
2013-11-04 11:17:46 -0800

    Trying to get code onto github...

--- Packet/Packet.h
+++ Packet/Packet.h
@@ -7,28 +7,30 @@
 #include "Flow.h"
 
 class Packet{
-    private:
-        enum PacketType {
-            INF,
-            DATA,
-            ACK
-        };
-        char *data;
-        PacketType type;
-        std::string id;
-        Host *source;
-        Host *destination;
-        time_t startTime;
-        int size;
-        Flow *flowId;
 
-    public:
-        PacketType getType();
-        Packet(char *d, PacketType t, Host *s, Host *de, time_t start, 
-                int size, Flow *f, std::string id);
-        ~Packet();
-        const char * getData();
-        void setData(char * d);
+public:
+    enum PacketType {
+        ROUTE,
+        DATA,
+        ACK
+    };
+private:
+    char *data;
+    PacketType type;
+    std::string id;
+    Host *source;
+    Host *destination;
+    time_t startTime;
+    int size;
+    Flow *flowId;
+
+public:
+    PacketType getType();
+    Packet(char *d, PacketType t, Host *s, Host *de, time_t start, 
+            int size, Flow *f, std::string id);
+    ~Packet();
+    const char * getData();
+    void setData(char * d);
 
 };
 
