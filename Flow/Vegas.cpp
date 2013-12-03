b3959439827a9245ba88dcd49fcac3a40a965f25
Louis OBryan <obryanlouis@gmail.com>
2013-12-02 21:55:45 -0800

    Vegas

---
+++ Flow/Vegas.cpp
@@ -0,0 +1,5 @@
+#include "Vegas.h"
+
+const double ALPHA = 10;
+const double BETA = 13;
+
---
+++ Flow/Vegas.h
@@ -0,0 +1,12 @@
+#ifndef VEGAS_H
+#define VEGAS_H
+
+#include "CongestionAlgorithm.h"
+
+class Vegas : public TCP_TAHOE {
+    private:
+    public:
+ 
+};
+
+#endif
